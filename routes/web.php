3afa296561448861111f66d242ae896cfe9719a8
ILAYA BARATHI M <119613096+webzacon@users.noreply.github.com>
2026-02-26 13:48:46 +0530

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+﻿<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'index')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/services', 'service')->name('services');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/pricing', 'pricing')->name('pricing');
+
